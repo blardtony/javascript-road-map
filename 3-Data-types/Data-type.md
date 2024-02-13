c2e9502ae2b60c424bd2ba075a68310e2fcf9671
Tony Blard <tonyblard55@gmail.com>
2024-02-13 14:36:36 +0100

    Feat : Primitives and object datatype

---
+++ 3-Data-types/Data-type.md
@@ -0,0 +1,22 @@
+# Data-types
+
+## Primitives and objects
+
+JavaScript divides data essentially into two main categories: primitives and objects.
+
+Primitives Datatype
+
+- String
+- Number
+- Bigint
+- Boolean
+- Undefined
+- Null
+- Symbol
+- Object
+
+Object Datatype
+
+- Object
+- Array
+- Date
---
+++ 3-Data-types/index.js
@@ -0,0 +1,14 @@
+/** Primitives and objects */
+
+const number = 1; // number
+const bigInt = 1234567890123456789012345678901234567890n; // bigInt
+const string = "string"; // string
+const boolean = true; // boolean
+const nullValue = null; // null
+const undefinedValue = undefined; // undefined
+const sym1 = Symbol("Foo"); // symbol
+
+const object = { key: "value" }; // object
+const array = [1, 2, 3]; // array
+const date = new Date(); // date
+const functionValue = function () {}; // function
